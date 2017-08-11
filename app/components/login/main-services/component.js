ac7f58c79852ad816da623bd85d30c8b4b664477
mateuszRybczonek <m.rybczonek@gmail.com>
2017-08-11 11:03:22 +0200

    main-services item component added

--- app/components/login/main-services/component.js
+++ app/components/login/main-services/component.js
@@ -4,4 +4,26 @@ const { Component } = Ember;
 
 export default Component.extend({
   tagName: ['section'],
+
+  items: [
+    {
+      icon: 'backup',
+      title: 'Access',
+      descriptionItems: [
+        'World-wide cover'
+      ],
+    }, {
+      icon: 'assignment',
+      title: 'Reports',
+      descriptionItems: [
+        'Remotely reports'
+      ],
+    }, {
+      icon: 'assessment',
+      title: 'Statistics',
+      descriptionItems: [
+        'Track performance'
+      ],
+    },
+  ],
 });
