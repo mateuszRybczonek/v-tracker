a14a7342be10ab52678c6dbeddf29b037e0ed750
mateuszRybczonek <m.rybczonek@gmail.com>
2017-08-22 08:21:18 +0200

    uploading image of the vessel

--- app/components/vessels/vessel-form/component.js
+++ app/components/vessels/vessel-form/component.js
@@ -19,6 +19,30 @@ export default Component.extend({
     return `${(this.get('progressValue') * 100).toFixed(0)}%`;
   }),
 
+  imagePath: computed('vessel', 'session.currentUser.uid', function() {
+    const vesselId = this.get('vessel.id');
+    const userId = this.get('session.currentUser.uid');
+    return `${userId}/vessels/${vesselId}.jpg`;
+  }),
+
+  imageUrl: computed('imagePath', function() {
+    const storageRef = this.get('firebaseApp').storage().ref();
+    storageRef.child(this.get('imagePath')).getDownloadURL().then((url) => {
+      this.set('imageUrl', url);
+    }).catch((error) => {
+      switch (error.code) {
+        case 'storage/object_not_found':
+          break;
+        case 'storage/unauthorized':
+          break;
+        case 'storage/canceled':
+          break;
+        case 'storage/unknown':
+          break;
+      }
+    });
+  }),
+
   isInvalid: computed.not('vessel.isValid'),
 
   actions: {
--- app/components/vessels/vessels-list/item/component.js
+++ app/components/vessels/vessels-list/item/component.js
@@ -1,7 +1,42 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const { Component, computed, inject: { service } } = Ember;
 
 export default Component.extend({
   classNames: ['vessels-list-item'],
+
+  firebaseApp: service(),
+  storageRef: '',
+  file: '',
+  router: service('-routing'),
+
+  imagePath: computed('vessel', 'session.currentUser.uid', function() {
+    const vesselId = this.get('vessel.id');
+    const userId = this.get('session.currentUser.uid');
+    return `${userId}/vessels/${vesselId}.jpg`;
+  }),
+
+  imageUrl: computed('imagePath', function() {
+    const storageRef = this.get('firebaseApp').storage().ref();
+    storageRef.child(this.get('imagePath')).getDownloadURL().then((url) => {
+      this.set('imageUrl', url);
+    }).catch((error) => {
+      switch (error.code) {
+        case 'storage/object_not_found':
+          break;
+        case 'storage/unauthorized':
+          break;
+        case 'storage/canceled':
+          break;
+        case 'storage/unknown':
+          break;
+      }
+    });
+  }),
+
+  actions: {
+    transitionToEdit() {
+      this.get('router').transitionTo('users.vessels.edit', [this.get('vessel.id')]);
+    },
+  }
 });
